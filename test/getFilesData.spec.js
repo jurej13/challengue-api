c7346c83b4809e928e5ac5d2c6155334ed3081ab
Javier Jure <jjure13@gmail.com>
2024-11-06 00:47:25 -0300

    change in test

--- test/getFilesData.spec.js
+++ test/getFilesData.spec.js
@@ -4,6 +4,9 @@ import { getFilesData } from "../controllers/files.js"
 describe("getFilesData", function () {
 	it("should return structured data if files are retrieved and parsed correctly", async function () {
 		const req = {
+			query: {
+				fileName: "test.csv",
+			},
 			testData: [
 				{
 					file: "test.csv",
@@ -23,6 +26,7 @@ describe("getFilesData", function () {
 				expect(data).to.be.an("array")
 				expect(data[0]).to.have.property("file")
 				expect(data[0]).to.have.property("lines")
+				expect(data[0].file).to.equal("test.csv")
 			},
 			status(code) {
 				expect(code).to.equal(200)
@@ -32,10 +36,23 @@ describe("getFilesData", function () {
 
 		await getFilesData(req, res)
 	})
-
 	it("should handle errors and return status 500", async function () {
 		const req = {
-			testData: [],
+			query: {
+				fileName: "nonexistent.csv",
+			},
+			testData: [
+				{
+					file: "test.csv",
+					lines: [
+						{
+							text: "test text",
+							number: 123,
+							hex: "0123456789abcdef0123456789abcdef",
+						},
+					],
+				},
+			],
 		}
 
 		const res = {
